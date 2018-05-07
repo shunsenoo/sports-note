ff9603470ebb0599c67d6772b46ce793c182dcb0
妹尾駿 <senooshun@senooshun-no-macbook-air.local>
2018-05-07 14:26:39 +0900

    Initial Commit

---
+++ OriginalApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OriginalApp
+//
+//  Created by 妹尾駿 on H30/05/07.
+//  Copyright © 平成30年 porme.inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
